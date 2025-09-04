9096885cab3dea6650376db1043fe1a573b07afb
Tayyab Shahid <34285753+tayyabshahid7@users.noreply.github.com>
2025-09-04 19:58:27 +0500

    Add enhanced stealth options to bypass Cloudflare detection
    - Add more Chrome options to make browser less detectable
    - Add random delays to mimic human behavior
    - Override navigator properties to hide automation
    - Add realistic user agent string
    - Disable various detection features

--- api_server.py
+++ api_server.py
@@ -5,6 +5,7 @@ import time
 import os
 import sys
 import uuid
+import random
 from datetime import datetime
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -197,13 +198,26 @@ def run_automation(session_id, username, password, card_number, card_expired_mon
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
         options.add_argument('--disable-blink-features=AutomationControlled')
+        options.add_argument('--disable-web-security')
+        options.add_argument('--disable-features=VizDisplayCompositor')
+        options.add_argument('--disable-extensions')
+        options.add_argument('--disable-plugins')
+        options.add_argument('--disable-images')
+        options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36')
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option('useAutomationExtension', False)
+        options.add_experimental_option("detach", True)
         
         driver = webdriver.Chrome(service=service, options=options)
         
-        # Execute script to remove webdriver property
+        # Execute scripts to make browser less detectable
         driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        driver.execute_script("Object.defineProperty(navigator, 'plugins', {get: () => [1, 2, 3, 4, 5]})")
+        driver.execute_script("Object.defineProperty(navigator, 'languages', {get: () => ['en-US', 'en']})")
+        driver.execute_script("Object.defineProperty(navigator, 'permissions', {get: () => ({query: () => Promise.resolve({state: 'granted'})})})")
+        
+        # Add random delays to mimic human behavior
+        time.sleep(random.uniform(2, 4))
         
         session['driver'] = driver
         
